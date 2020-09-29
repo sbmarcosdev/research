d9f86c5c34e3e67a91a23bde3851adc0329ffef8
marcos.msilva <marcos.msilva@EMBRACON.LOCAL>
2020-09-29 09:51:00 -0300

    rel_desc

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -14,12 +14,12 @@ class RelatorioController extends Controller
     public function index()
     {
         $rel = Campanha::all();
-        
-        return view('relatorios.index', compact('rel'));  
+
+        return view('relatorios.index', compact('rel'));
     }
 
     public function show($campanha_id)
-    {  
+    {
         $sql = "SELECT pergunta_id,
                        (SELECT texto from perguntas where id = pergunta_id) as pergunta,
                        ROUND(sum( peso_resposta ) / (count(id) * 5) * 100)  as peso,
@@ -31,36 +31,42 @@ class RelatorioController extends Controller
         $rel = DB::select($sql);
 
         $campanha = Campanha::find($campanha_id);
-        
-        return view('relatorios.show', compact('rel','campanha'));
+
+        return view('relatorios.show', compact('rel', 'campanha'));
     }
 
     public function detalhe($pergunta_id)
-    {        
+    {
         $pergunta = Pergunta::find($pergunta_id);
-        
-        $opcoes = OpcaoResposta::where('tipo_id', $pergunta->tipo_id)->get();
-        
-        foreach ($opcoes as $opcao){
 
-        $detalhes = Resposta::where('pergunta_id', $pergunta_id)
-                                ->where('tipo_id', $pergunta->tipo_id)
-                                ->where('peso_resposta', $opcao->peso)
-                                ->count();
+        if ($pergunta->tipo_id == 5) {
 
-            $sub[$opcao->peso]=$detalhes;
+            $respostas = Resposta::where('pergunta_id', $pergunta_id)->get();
+
+            return view('relatorios.descritiva', compact('respostas', 'pergunta'));
+
+        } else {
+            $opcoes = OpcaoResposta::where('tipo_id', $pergunta->tipo_id)->get();
+            
+            foreach ($opcoes as $opcao) {
+                $detalhes = Resposta::where('pergunta_id', $pergunta_id)
+                    ->where('tipo_id', $pergunta->tipo_id)
+                    ->where('peso_resposta', $opcao->peso)
+                    ->count();
+
+                $sub[$opcao->peso] = $detalhes;
+            }
+            return view('relatorios.detalhes', compact('sub', 'opcoes', 'detalhes', 'pergunta'));
         }
-
-        return view('relatorios.detalhes', compact('sub','opcoes', 'detalhes','pergunta'));
     }
 
-    public function respostas ( $campanha_id, $respondente_id )
+    public function respostas($campanha_id, $respondente_id)
     {
-        $respondente = CampanhaRespondente::where('respondente_id', $respondente_id ) 
-                                   ->where('campanha_id',$campanha_id )
-                                   ->get();
+        $respondente = CampanhaRespondente::where('respondente_id', $respondente_id)
+            ->where('campanha_id', $campanha_id)
+            ->get();
 
-        $perguntas = Pergunta::where('campanha_id',$campanha_id)->get();
+        $perguntas = Pergunta::where('campanha_id', $campanha_id)->get();
 
         $opcoes = OpcaoResposta::where('tipo_id', 1)->get();
 
@@ -68,9 +74,9 @@ class RelatorioController extends Controller
 
             $resposta[$pergunta->id] = $pergunta->resposta->where('respondente_id', $respondente_id)
                 ->where('campanha_id', $campanha_id)->first();
-        }       
-        
+        }
 
-        return view('relatorios.respostas', compact('respondente','opcoes', 'resposta', 'perguntas'));
+
+        return view('relatorios.respostas', compact('respondente', 'opcoes', 'resposta', 'perguntas'));
     }
 }
---
+++ resources/views/relatorios/descritiva.blade.php
@@ -0,0 +1,39 @@
+@extends('template.template')
+@section('content')
+<div class="container">
+    <div class="container espacamento">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-body" style="min-height: 500px">
+                        <div class="col-sm-12">
+                            <div class="col-lg-12" style="text-align: right;">
+                                <button type="button" class="btn-info" onClick='window.history.back()'>Voltar</button>
+                            </div>
+                          
+                            <hr>
+                            <table class="table table-striped" style="width: 100%; margin: 0 auto;">
+                                <thead>
+                                    <tr>
+                                        <th> {{ $pergunta->texto }} </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($respostas as $resposta)
+                                    <tr>
+                                        <td>{{ $resposta->texto_resposta }}</td>
+                                    </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
