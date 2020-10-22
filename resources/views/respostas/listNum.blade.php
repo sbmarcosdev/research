be0cbd416c36d170b19fb0db0505e028e740e2fa
marcos.msilva <marcos.msilva@EMBRACON.LOCAL>
2020-10-22 14:54:37 -0300

    progress_margin

--- resources/views/respostas/listNum.blade.php
+++ resources/views/respostas/listNum.blade.php
@@ -59,7 +59,7 @@
             <div class="tituloPrincipal mt-3">
                 <button type="submit" class="btn btn-outline-info mb-4">Enviar Respostas</button>
             </div>
-            <div class="progress">
+            <div class="progress m-3">
                 <div class="progress-bar" role="progressbar" style="width: {{$progresso}}%;" aria-valuenow="{{$progresso}}" aria-valuemin="0" aria-valuemax="100">{{$qtd}}</div>
             </div>
     </div>
